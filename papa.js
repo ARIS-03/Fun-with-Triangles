dcb849740289c3c1e7b6dbd2653e3f8bc91140d4
ARIS-03 <arisahmed0426@gmail.com>
2021-09-04 15:59:39 +0530

    hypotenuse

---
+++ papa.js
@@ -0,0 +1,17 @@
+// const side = document.querySelectorAll('.side-input');
+//   const hypobtn = document.querySelector('#hypo-btn');
+//   const outputE1 =  document.querySelector('#output');
+
+//   function sumofsquares(a,b) {
+//   const sumofsides  = a*a + b*b;
+//   return sumofsides;
+
+//   }
+
+//   function calHypo(){
+
+//     const sumofsides = sumofsquares(Number(side[0].value),Number(side[1].value));
+//     const hypo = Math.sqrt(sumofsides);
+//     outputE1.innerText = "The length of hypotenuse is" + hypo
+//   }
+//   hypobtn.addEventListener("click",calHypo);
